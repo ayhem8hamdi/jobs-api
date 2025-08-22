b3c0fc54b60ff7b0eb814d4f5c866630683a21b6
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-08-22 19:55:23 +0100

    create user model + joi validation

---
+++ models/user-model.js
@@ -0,0 +1,10 @@
+const { required } = require("joi");
+const mongoose = require("mongoose");
+
+const userSchema= new mongoose.Schema(
+    {
+        username : {type: String, required: true , trim: true},
+        email :  {type: String, required: true , trim: true},
+        password :  {type: String, required: true , trim: true},
+    },{timestamps: true}
+);
