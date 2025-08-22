16b01ec166dd10e35ae061f8f8660a30a9da7e91
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-08-22 20:04:27 +0100

    create password hashing method and use pre save mongoose mw to hash password

--- models/user-model.js
+++ models/user-model.js
@@ -1,6 +1,9 @@
-const { required } = require("joi");
+const  Joi  = require("joi");
 const mongoose = require("mongoose");
+const {hashPassword}= require("../helper/password-hashing");
 
+
+// Schema 
 const userSchema= new mongoose.Schema(
     {
         username : {type: String, required: true , trim: true},
@@ -8,3 +11,62 @@ const userSchema= new mongoose.Schema(
         password :  {type: String, required: true , trim: true},
     },{timestamps: true}
 );
+
+
+// password hashing 
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  // If hashPassword fails, Mongoose will reject save with the error
+  this.password = await hashPassword(this.password);
+});
+
+// jwt generation 
+userSchema.methods.generateToken = function () {
+  return jwt.sign(
+    {userId: this._id},
+    process.env.JWT_SECRET_KEY
+  );
+};
+
+
+// register validation 
+
+const registerValidation = Joi.object({
+  username: Joi.string()
+    .min(3)                
+    .max(30)               
+    .required()
+    .trim(),
+
+  email: Joi.string()
+    .email({ tlds: { allow: false } }) 
+    .required()
+    .trim(),
+
+  password: Joi.string()
+    .min(6)               
+    .required()
+    .trim(),
+});
+
+//login valdiation 
+
+const loginValidation = Joi.object(
+    {
+        email: Joi.string()
+    .email({ tlds: { allow: false } }) 
+    .required()
+    .trim(),
+
+  password: Joi.string()
+    .min(6)               
+    .required()
+    .trim(),  
+    }
+); 
+
+
+// model creation
+const userModel = mongoose.model("User",userSchema);
+
+module.exports = {userModel,registerValidation,loginValidation};
